42a1ed9c51e1ae048edb89f5d3523b7d0cbae971
yxl <yangxulei84@qq.com>
2017-09-06 22:21:41 +0800

    add 自动生成文本

---
+++ 自动生成文本/generate.py
@@ -0,0 +1,137 @@
+#coding:utf-8
+import tensorflow as tf
+import sys,time
+import numpy as np
+import _pickle as cPickle
+import os
+import random
+import Config
+import Model
+
+config_tf = tf.ConfigProto()
+config_tf.gpu_options.allow_growth = True
+config_tf.inter_op_parallelism_threads = 1
+config_tf.intra_op_parallelism_threads = 1
+
+config = Config.Config()
+
+char_to_idx, idx_to_char = cPickle.load(open(config.model_path+'.voc', 'r'))
+
+config.vocab_size = len(char_to_idx)
+is_sample = config.is_sample
+is_beams = config.is_beams
+beam_size = config.beam_size
+len_of_generation = config.len_of_generation
+start_sentence = config.start_sentence
+
+def run_epoch(session, m, data, eval_op, state=None):
+    """Runs the model on the given data."""
+    x = data.reshape((1,1))
+    prob, _state, _ = session.run([m._prob, m.final_state, eval_op],
+                         {m.input_data: x,
+                          m.initial_state: state})
+    return prob, _state
+
+def main(_):
+    with tf.Graph().as_default(), tf.Session(config=config_tf) as session:
+        config.batch_size = 1
+        config.num_steps = 1
+
+        initializer = tf.random_uniform_initializer(-config.init_scale,
+                                                config.init_scale)
+        with tf.variable_scope("model", reuse=None, initializer=initializer):
+            mtest = Model.Model(is_training=False, config=config)
+
+        #tf.global_variables_initializer().run()
+
+        model_saver = tf.train.Saver()
+        print('model loading ...')
+        model_saver.restore(session, config.model_path+'-%d'%config.save_time)
+        print('Done!')
+
+        if not is_beams:
+            # sentence state
+            char_list = list(start_sentence);
+            start_idx = char_to_idx[char_list[0]]
+            _state = mtest.initial_state.eval()
+            test_data = np.int32([start_idx])
+            prob, _state = run_epoch(session, mtest, test_data, tf.no_op(), _state)
+            gen_res = [char_list[0]]
+            for i in xrange(1, len(char_list)):
+                char = char_list[i]
+                try:
+                    char_index = char_to_idx[char]
+                except KeyError:
+                    char_index = np.argmax(prob.reshape(-1))
+                prob, _state = run_epoch(session, mtest, np.int32([char_index]), tf.no_op(), _state)
+                gen_res.append(char)
+            # gen text
+            if is_sample:
+                gen = np.random.choice(config.vocab_size, 1, p=prob.reshape(-1))
+                gen = gen[0]
+            else:
+                gen = np.argmax(prob.reshape(-1))
+            test_data = np.int32(gen)
+            gen_res.append(idx_to_char[gen])
+            for i in range(len_of_generation-1):
+                prob, _state = run_epoch(session, mtest, test_data, tf.no_op(), _state)
+                if is_sample:
+                    gen = np.random.choice(config.vocab_size, 1, p=prob.reshape(-1))
+                    gen = gen[0]
+                else:
+                    gen = np.argmax(prob.reshape(-1))
+                test_data = np.int32(gen)
+                gen_res.append(idx_to_char[gen])
+            print('Generated Result: ',''.join(gen_res))
+        else:
+            # sentence state
+            char_list = list(start_sentence);
+            start_idx = char_to_idx[char_list[0]]
+            _state = mtest.initial_state.eval()
+            beams = [(0.0, [idx_to_char[start_idx]], idx_to_char[start_idx])]
+            test_data = np.int32([start_idx])
+            prob, _state = run_epoch(session, mtest, test_data, tf.no_op(), _state)
+            y1 = np.log(1e-20 + prob.reshape(-1))
+            beams = [(beams[0][0], beams[0][1], beams[0][2], _state)]
+            for i in xrange(1, len(char_list)):
+                char = char_list[i]
+                try:
+                    char_index = char_to_idx[char]
+                except KeyError:
+                    top_indices = np.argsort(-y1)
+                    char_index = top_indices[0]
+                prob, _state = run_epoch(session, mtest, np.int32([char_index]), tf.no_op(), beams[0][3])
+                y1 = np.log(1e-20 + prob.reshape(-1))
+                beams = [(beams[0][0], beams[0][1] + [char], char_index, _state)]
+            # gen text
+            if is_sample:
+                top_indices = np.random.choice(config.vocab_size, beam_size, replace=False, p=prob.reshape(-1))
+            else:
+                top_indices = np.argsort(-y1)
+            b = beams[0]
+            beam_candidates = []
+            for i in xrange(beam_size):
+                wordix = top_indices[i]
+                beam_candidates.append((b[0] + y1[wordix], b[1] + [idx_to_char[wordix]], wordix, _state))
+            beam_candidates.sort(key = lambda x:x[0], reverse = True) # decreasing order
+            beams = beam_candidates[:beam_size] # truncate to get new beams
+            for xy in range(len_of_generation-1):
+                beam_candidates = []
+                for b in beams:
+                    test_data = np.int32(b[2])
+                    prob, _state = run_epoch(session, mtest, test_data, tf.no_op(), b[3])
+                    y1 = np.log(1e-20 + prob.reshape(-1))
+                    if is_sample:
+                        top_indices = np.random.choice(config.vocab_size, beam_size, replace=False, p=prob.reshape(-1))
+                    else:
+                        top_indices = np.argsort(-y1)
+                    for i in xrange(beam_size):
+                        wordix = top_indices[i]
+                        beam_candidates.append((b[0] + y1[wordix], b[1] + [idx_to_char[wordix]], wordix, _state))
+                beam_candidates.sort(key = lambda x:x[0], reverse = True) # decreasing order
+                beams = beam_candidates[:beam_size] # truncate to get new beams
+
+            print('Generated Result: ',''.join(beams[0][1]))
+
+if __name__ == "__main__":
+    tf.app.run()
---
+++ 自动生成文本/train.py
@@ -0,0 +1,103 @@
+# coding:utf-8
+import tensorflow as tf
+import sys, time
+import numpy as np
+import _pickle as cPickle
+import Config
+import Model
+
+config_tf = tf.ConfigProto()
+config_tf.gpu_options.allow_growth = True
+config_tf.inter_op_parallelism_threads = 1
+config_tf.intra_op_parallelism_threads = 1
+
+file = sys.argv[1]
+data = open(file, 'r').read()
+data = data.decode('utf-8')
+
+chars = list(set(data))  # char vocabulary
+
+data_size, _vocab_size = len(data), len(chars)
+print('data has %d characters, %d unique.' % (data_size, _vocab_size))
+char_to_idx = {ch: i for i, ch in enumerate(chars)}
+idx_to_char = {i: ch for i, ch in enumerate(chars)}
+
+config = Config.Config()
+config.vocab_size = _vocab_size
+
+cPickle.dump((char_to_idx, idx_to_char), open(config.model_path + '.voc', 'w'), protocol=cPickle.HIGHEST_PROTOCOL)
+
+context_of_idx = [char_to_idx[ch] for ch in data]
+
+
+def data_iterator(raw_data, batch_size, num_steps):
+    raw_data = np.array(raw_data, dtype=np.int32)
+
+    data_len = len(raw_data)
+    batch_len = data_len // batch_size
+    data = np.zeros([batch_size, batch_len], dtype=np.int32)
+    for i in range(batch_size):
+        data[i] = raw_data[batch_len * i:batch_len * (i + 1)]  # data的shape是(batch_size, batch_len)，每一行是连贯的一段，一次可输入多个段
+
+    epoch_size = (batch_len - 1) // num_steps
+
+    if epoch_size == 0:
+        raise ValueError("epoch_size == 0, decrease batch_size or num_steps")
+
+    for i in range(epoch_size):
+        x = data[:, i * num_steps:(i + 1) * num_steps]
+        y = data[:, i * num_steps + 1:(i + 1) * num_steps + 1]  # y就是x的错一位，即下一个词
+        yield (x, y)
+
+
+def run_epoch(session, m, data, eval_op):
+    """Runs the model on the given data."""
+    epoch_size = ((len(data) // m.batch_size) - 1) // m.num_steps
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = m.initial_state.eval()
+    for step, (x, y) in enumerate(data_iterator(data, m.batch_size,
+                                                m.num_steps)):
+        cost, state, _ = session.run([m.cost, m.final_state, eval_op],  # x和y的shape都是(batch_size, num_steps)
+                                     {m.input_data: x,
+                                      m.targets: y,
+                                      m.initial_state: state})
+        costs += cost
+        iters += m.num_steps
+
+        if step and step % (epoch_size // 10) == 0:
+            print("%.2f perplexity: %.3f cost-time: %.2f s" %
+                  (step * 1.0 / epoch_size, np.exp(costs / iters),
+                   (time.time() - start_time)))
+            start_time = time.time()
+
+    return np.exp(costs / iters)
+
+
+def main(_):
+    train_data = context_of_idx
+
+    with tf.Graph().as_default(), tf.Session(config=config_tf) as session:
+        initializer = tf.random_uniform_initializer(-config.init_scale,
+                                                    config.init_scale)
+        with tf.variable_scope("model", reuse=None, initializer=initializer):
+            m = Model.Model(is_training=True, config=config)
+
+        tf.global_variables_initializer().run()
+
+        model_saver = tf.train.Saver(tf.global_variables())
+
+        for i in range(config.iteration):
+            print("Training Epoch: %d ..." % (i + 1))
+            train_perplexity = run_epoch(session, m, train_data, m.train_op)
+            print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+
+            if (i + 1) % config.save_freq == 0:
+                print('model saving ...')
+                model_saver.save(session, config.model_path + '-%d' % (i + 1))
+                print('Done!')
+
+
+if __name__ == "__main__":
+    tf.app.run()
